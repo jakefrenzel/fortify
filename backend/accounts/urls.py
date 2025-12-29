0e3252988b59d7efb8150ef41173dd64f800fdaa
jakefrenzel <jakef1@hotmail.com>
2025-12-28 20:42:39 -0700

    Fortify v0.0.6 [Added - URLs added for JWT authentication]

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from accounts.views import LoginView, LogoutView, RefreshView
 
 urlpatterns = [
     # Define your account-related URL patterns here
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('refresh/', RefreshView.as_view(), name='refresh'),
 ]
