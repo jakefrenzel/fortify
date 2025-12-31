2accf7c68407205cd7da1c3b92b5408932499c13
jakefrenzel <jakef1@hotmail.com>
2025-12-31 14:21:11 -0700

    Fortify v0.0.13 [Improved - Clarity in the url paths for auth]

--- backend/authenticator/urls.py
+++ backend/authenticator/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from authenticator.views import LoginView, LogoutView, RefreshView
+from authenticator.views import LoginView, LogoutView, RefreshTokenView
 
 urlpatterns = [
     # Define your account-related URL patterns here
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('refresh/', RefreshView.as_view(), name='refresh'),
+    path('auth/login/', LoginView.as_view(), name='login'),
+    path('auth/logout/', LogoutView.as_view(), name='logout'),
+    path('auth/refresh/', RefreshTokenView.as_view(), name='refresh'),
 ]
